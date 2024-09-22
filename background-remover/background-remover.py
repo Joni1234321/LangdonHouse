36beac7767afe1a40af28b77e080725a2fe04ea2
jvh <Joni1234@hotmail.dk>
2024-09-22 19:09:23 +0200

    updated remover

--- background-remover/background-remover.py
+++ background-remover/background-remover.py
@@ -3,16 +3,16 @@ import rembg
 
 def process (input_path: str, output_path: str, session = rembg.new_session()):
     if not os.path.exists(input_path):
-        print(f"ERROR: input file does not exist {input_path}")
+        print(f"ERROR: input file does not exist [{input_path}]")
         return False
     
-    output_dir_path = os.path.dirname(input_path)
+    output_dir_path = os.path.dirname(output_path)
     if not os.path.isdir(output_dir_path):
-        print(f"ERROR: output directory does not exist {output_dir_path} ")
+        print(f"ERROR: output directory does not exist [{output_dir_path}] ")
         return False
 
     with open(input_path, 'rb') as i:
-        with open(output_path, 'wb', create_parents=True) as o:
+        with open(output_path, 'wb') as o:
             input = i.read()
             output = rembg.remove(input, session=session)
             o.write(output)
