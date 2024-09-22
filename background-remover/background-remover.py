8b5ff7d8b8f68dbe5a5d52ad4ef2511209b8440d
jvh <Joni1234@hotmail.dk>
2024-09-22 18:05:32 +0200

    added check before

--- background-remover/background-remover.py
+++ background-remover/background-remover.py
@@ -1,8 +1,14 @@
+import os
 import rembg
 
 def process (input_path: str, output_path: str, session = rembg.new_session()):
+    if not os.path.exists(input_path) or not os.path.exists(output_path):
+        return False
+
     with open(input_path, 'rb') as i:
         with open(output_path, 'wb') as o:
             input = i.read()
             output = rembg.remove(input, session=session)
             o.write(output)
+
+    return True
