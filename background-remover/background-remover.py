22c457501c4c301ecb564099cf8b12d8c91eca68
jvh <Joni1234@hotmail.dk>
2024-09-22 18:56:37 +0200

    added error messages

--- background-remover/background-remover.py
+++ background-remover/background-remover.py
@@ -2,13 +2,19 @@ import os
 import rembg
 
 def process (input_path: str, output_path: str, session = rembg.new_session()):
-    if not os.path.exists(input_path) or not os.path.exists(output_path):
+    if not os.path.exists(input_path):
+        print(f"ERROR: input file does not exist {input_path}")
+        return False
+    
+    output_dir_path = os.path.dirname(input_path)
+    if not os.path.isdir(output_dir_path):
+        print(f"ERROR: output directory does not exist {output_dir_path} ")
         return False
 
     with open(input_path, 'rb') as i:
-        with open(output_path, 'wb') as o:
+        with open(output_path, 'wb', create_parents=True) as o:
             input = i.read()
             output = rembg.remove(input, session=session)
             o.write(output)
 
-    return True
+    return False
