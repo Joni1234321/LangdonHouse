326229a19c8cab1934eac9c65cd90033a7b0302d
JVH <Joni1234@hotmail.dk>
2024-09-28 17:51:56 +0200

    converts all non converted file in folders, optiamization and error handling

--- background-remover/background-remover.py
+++ background-remover/background-remover.py
@@ -1,20 +1,54 @@
-import os
+import os, glob
 import rembg
 
-def process (input_path: str, output_path: str, session = rembg.new_session()):
+def process (input_path: str, output_path: str, session = rembg.new_session()) -> bool:
+    print(f"making file at {input_path} to {output_path}")
     if not os.path.exists(input_path):
-        print(f"ERROR: input file does not exist [{input_path}]")
+        print(f"ERROR: Process failed due to input file does not exist [{input_path}]")
         return False
-    
+        
     output_dir_path = os.path.dirname(output_path)
     if not os.path.isdir(output_dir_path):
-        print(f"ERROR: output directory does not exist [{output_dir_path}] ")
-        return False
+        print(f"LOG: Process creates output directory [{output_dir_path}].")
+        os.makedirs(output_dir_path)
 
-    with open(input_path, 'rb') as i:
-        with open(output_path, 'wb') as o:
+    try:
+        with open(input_path, 'rb') as i:
             input = i.read()
-            output = rembg.remove(input, session=session)
+
+        output = rembg.remove(input, session=session)
+
+        with open(output_path, 'wb') as o:
             o.write(output)
 
-    return False
+    except Exception as error:
+        print(f"ERROR: Process failed due to exception [{error}]")
+        return False
+
+    return True
+
+# Getting files
+in_dir  = f"{os.path.dirname(__file__)}/test/in"
+out_dir = f"{os.path.dirname(__file__)}/test/out"
+
+if __name__ == '__main__':
+    print("LOG: Started running, looking for file")
+    
+    in_files  =      [os.path.splitext(os.path.relpath(p, start=in_dir))  for p in glob.glob(f"{in_dir}/**/*.*",  recursive=True)]
+    out_files = dict([os.path.splitext(os.path.relpath(p, start=out_dir)) for p in glob.glob(f"{out_dir}/**/*.*", recursive=True)])
+
+    missing_in_files = [(stem, ext) for (stem, ext) in in_files if stem not in out_files]
+
+    print(f"LOG: Files to process [{len(missing_in_files)}]")
+
+    failed = 0
+    for (stem, ext) in missing_in_files:
+        in_file  = os.path.join(in_dir,  f"{stem}{ext}")
+        out_file = os.path.join(out_dir, f"{stem}.png")
+        if not process(in_file, out_file):
+            failed += 1
+    
+    print(f"LOG: Files succeeded  [{len(missing_in_files) - failed}]")
+    print(f"LOG: Files failed     [{failed}]")
+    
+    print('LOG: Stopping Background Process')
